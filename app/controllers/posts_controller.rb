98de216f1c0ca04de576b94d3115ab83cc753502
Tabuchi Hisatoshipro <Tabuchipro@tabuchinoMacBook-puro.local>
2019-05-09 13:03:07 +0900

    CRUD機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,11 +14,20 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
-    @post = Post.new
+    if params[:back]
+      @post = Post.new(post_params)
+    else
+      @post = Post.new
+    end
   end
 
   # GET /posts/1/edit
   def edit
+  end
+
+  def confirm
+    @post=Post.new(post_params)
+    render 'new' if @post.invalid?
   end
 
   # POST /posts
@@ -51,14 +60,9 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to posts_path, notice: 'Post was successfully destroyed.'
   end
 
   private
@@ -69,6 +73,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.fetch(:post, {})
+      params.fetch(:post, {}).permit(:content)
     end
 end
---
+++ db/migrate/20190509031117_add_column_to_post.rb
@@ -0,0 +1,6 @@
+class AddColumnToPost < ActiveRecord::Migration[5.1]
+  def change
+    add_column :posts, :content, :text
+    add_column :posts, :image, :string
+  end
+end
