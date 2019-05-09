36e59cca002ec1d1821e3a49a0ffe07cf2ff55fd
Tabuchi Hisatoshipro <Tabuchipro@tabuchinoMacBook-puro.local>
2019-05-09 14:48:25 +0900

    ファイルアップロード機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,19 +30,12 @@ class PostsController < ApplicationController
     render 'new' if @post.invalid?
   end
 
-  # POST /posts
-  # POST /posts.json
   def create
     @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render 'new'
     end
   end
 
@@ -73,6 +66,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.fetch(:post, {}).permit(:content)
+      params.fetch(:post, {}).permit(:content,:image,:image_cache)
     end
 end
