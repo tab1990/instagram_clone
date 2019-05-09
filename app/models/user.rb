12aae53f2970bd93322aaa0567097c89ed41ab63
Tabuchi Hisatoshipro <Tabuchipro@tabuchinoMacBook-puro.local>
2019-05-09 17:14:24 +0900

    ログイン機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  before_validation {email.downcase!}
+  validates :name,presence: true,length:{maximum: 30}
+  validates :email,presence: true,length:{maximum: 255},format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },uniqueness: true
+  has_secure_password
+  validates :password,length: {minimum: 6}
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users,only:[:new,:create,:show]
   resources :posts do
     collection do
       post :confirm
