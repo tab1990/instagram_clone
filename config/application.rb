5a63bcede99dd774a4a128dd1dfc8f0bb58de372
Tabuchi Hisatoshipro <Tabuchipro@tabuchinoMacBook-puro.local>
2019-05-10 19:21:46 +0900

    タイムゾーン、rails g コマンドの設定

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,16 @@ module InstagramCloneApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.assets false
+      g.jbuilder false
+    end
+
   end
 end
